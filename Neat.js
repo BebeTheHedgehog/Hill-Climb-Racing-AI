a3f58f2826b32ffe26aafc581dcfab343735a4ac
Bebe <110131572+BebeTheHedgehog@users.noreply.github.com>
2022-10-06 07:50:02 -0500

    Neat.js

--- Neat.js
+++ Neat.js
@@ -912,7 +912,7 @@ var nextPanX = 0;
 
 var nextConnectionNo = 1000;
 var population;
-var speed = 40;
+var speed = 75;
 
 
 var showBest = false; //true if only show the best of the previous generation
@@ -946,13 +946,13 @@ var grassSprites = [];
 var otherWorld; // for human, gen replay, species, best
 var worlds = [];
 var grounds = [];
-var numberOfWorlds = 10;
-var playersPerWorld = 75;
+var numberOfWorlds = 25;
+var playersPerWorld = 15;
 
 
 var skySprite;
 var darknessSprite;
-var difficulty = 50;
+var difficulty = 75;
 
 listener.BeginContact = function(contact) {
   let world = contact.GetFixtureA().GetBody().GetWorld();
